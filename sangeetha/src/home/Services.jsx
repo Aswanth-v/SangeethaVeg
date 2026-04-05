9c5e1e8a4f62dd740c39ec5f570c387bc1e46969
Aswabnth.v <aswanthv695@gmail.com>
2026-04-05 07:54:46 -0700

    created Services componet

---
+++ sangeetha/src/home/Services.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Services = () => {
+  return (
+    <div className="relative min-h-screen bg-cream px-4 sm:px-6 md:px-10 py-12">
+      <div className="absolute inset-0 bg-black/40 z-0"></div>
+      {/*left section */}
+      <div></div>
+      {/*Right section */}
+      <div></div>
+    </div>
+  );
+};
+
+export default Services;
--- sangeetha/src/pages/Homepage.jsx
+++ sangeetha/src/pages/Homepage.jsx
@@ -1,21 +1,20 @@
-import React from 'react'
-import Section from '../ui/section'
-import Navbar from '../layout/Navbar'
-import Hero from '../home/Hero'
-import FestivelSpecial from '../home/FestivelSpecial'
-import Clasicfood from '../home/Clasicfood'
+import React from "react";
+import Section from "../ui/section";
+import Navbar from "../layout/Navbar";
+import Hero from "../home/Hero";
+import FestivelSpecial from "../home/FestivelSpecial";
+import Clasicfood from "../home/Clasicfood";
+import Services from "../home/Services";
 const Homepage = () => {
   return (
     <div>
-    
-        <Navbar/>
-       <Hero/>
-       <FestivelSpecial/>
-       <Clasicfood/>
-     
+      <Navbar />
+      <Hero />
+      <FestivelSpecial />
+      <Clasicfood />
+      <Services />
     </div>
-     
-  )
-}
+  );
+};
 
-export default Homepage
+export default Homepage;
